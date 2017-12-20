73a5d59beff7b2969c062d28e19c267610f83a79
phantomspecial <win.2000@live.jp>
2017-12-20 17:08:57 +0900

    Add-_item-Edit,Delete-function

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -4,5 +4,17 @@ class StocksController < ApplicationController
     @stocks = Stock.all
   end
 
+  def destroy
+    stock = Stock.find(params[:id])
+    # nameを削除しますか？この操作は元に戻せませんのアラート
+    # [はい]がクリックしたときの動作
+    if stock.current_stock == 0
+      stock.destroy
+    else
+      # 在庫が０でないため、削除できませんのアラートを出す
+    end
+    redirect_to action: :index
+  end
+
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,3 @@
 class Item < ApplicationRecord
-  belongs_to :stock, foreign_key: "item_id"
-
   mount_uploader :image, ImageUploader
 end
