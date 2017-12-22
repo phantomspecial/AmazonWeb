f2d61220af58b6902ee84c54b4cd78dc90364879
phantomspecial <win.2000@live.jp>
2017-12-22 14:50:41 +0900

    Add-403-Error-view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,12 +8,27 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :nickname, :postal_code, :pref, :city, :street, :apartment_roomnumber, :telnumber])
   end
 
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_url, :alert => exception.message
+  end
+
   def after_sign_in_path_for(resource)
     root_path
   end
 
   def after_sign_out_path_for(resource)
     root_path
+  end
+
+  class Forbidden < ActionController::ActionControllerError; end
+  class IpAddressRejected < ActionController::ActionControllerError; end
+
+  # include ErrorHandlers if Rails.env.production? or Rails.env.staging?
+
+  class ApplicationController < ActionController::Base
+    rescue_from CanCan::AccessDenied do |exception|
+      redirect_to root_url, :alert => exception.message
+    end
   end
 
 end
