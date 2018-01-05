fcc9aca68a14eb41e799e73c61d455fb109e7182
phantomspecial <win.2000@live.jp>
2018-01-05 21:34:26 +0900

    Revise-logout-pageviews

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  # カートの数を取得
+  def gets_cart_number
+    @carts = current_user.carts.all
+    @totalitems = 0
+    @carts.each do |cart|
+      @totalitems += cart.quantity
+    end
+  end
 end
