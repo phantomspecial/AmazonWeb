64cb5db7cba041b68c173fb29bed0eea3107c439
Kate Gamo <keito.gamo@gmail.com>
2018-01-08 10:43:31 +0900

    delete of category index in admin page
    what: category index page
    why:change to “rake db:seed” way to show category index

---
+++ db/migrate/20180106080928_add_index_categories_name.rb
@@ -0,0 +1,5 @@
+class AddIndexCategoriesName < ActiveRecord::Migration[5.0]
+  def change
+    add_index :categories, :name, :unique => true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180105051820) do
+ActiveRecord::Schema.define(version: 20180106080928) do
 
   create_table "carts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "quantity",   null: false
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20180105051820) do
     t.text     "description", limit: 65535, null: false
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
+    t.index ["name"], name: "index_categories_on_name", unique: true, using: :btree
   end
 
   create_table "items", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
