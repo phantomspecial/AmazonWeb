ca84f1d60ff2ba53fcdbf3687be5e29726fe59eb
phantomspecial <win.2000@live.jp>
2018-01-10 18:32:43 +0900

    Revise-review-edit(1)_review-create

--- app/assets/javascripts/reviewedit.js
+++ app/assets/javascripts/reviewedit.js
@@ -1,11 +1,48 @@
 $(document).on('turbolinks:load', function() {
+  // $("#editreview").click(function()){
+  //    $(this).parent().siblings('#edit-modal').fadeIn();
+  // });
+  // $('.updbtn').on('click',function(){
+  //   $(this).parent().parent().parent('#edit-modal').fadeOut();
+  // });
 
-  $(".edit_review").click(function()){
-     $(this).parent().siblings('#edit-modal').fadeIn();
-  });
-  $('.updbtn').on('click',function(){
-    $(this).parent().parent().parent('#edit-modal').fadeOut();
-  });
+
+
+ $(".editreview").click(function(){
+    alert("AAA");
+      $("body").append('<div id="edit-modal"></div>');
+      modalResize();
+        $("#modal-bg,#edit-modal").fadeIn("slow");
+        $("#modal-bg").click(function(){
+          $("#edit-modal,#modal-bg").fadeOut("slow",function(){
+              $('#modal-bg').remove() ;
+          });
+        });
+      $(window).resize(modalResize);
+      function modalResize(){
+        var w = $(window).width();
+        var h = $(window).height();
+        var cw = $("#edit-modal").outerWidth();
+        var ch = $("#edit-modal").outerHeight();
+        $("#edit-modal").css({
+          "left": ((w - cw)/2) + "px",
+          "top": ((h - ch)/2) + "px"
+        });
+      }
+   });
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 });
 
@@ -14,7 +51,7 @@ $(document).on('turbolinks:load', function() {
 
 //   $(document).on('click','.edit_review' ,function(){
 //      $(this).parent().siblings('#edit-modal').fadeIn();
-//    });
+//    });,
 //   $('.updbtn').on('click',function(){
 //      $(this).parent().parent().parent('#edit-modal').fadeOut();
 //    });
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,9 +4,7 @@ class ReviewsController < ApplicationController
 
   def create
     @stock = Stock.find(review_params[:stock_id])
-    if review_valid_test(review_params) == true
-
-      @review = Review.create(stock_id: review_params[:stock_id], user_id: current_user.id, title: review_params[:title], rate: review_params[:rate], review: review_params[:review])
+    if @review = Review.create(stock_id: review_params[:stock_id], user_id: current_user.id, title: review_params[:title], rate: review_params[:rate], review: review_params[:review])
 
       @reviews = @stock.reviews
     else
@@ -40,14 +38,6 @@ class ReviewsController < ApplicationController
   def review_params
     params.permit(:stock_id, :title, :rate, :review)
     # params.require(:review).permit(:stock_id, :title, :rate, :review)
-  end
-
-  def review_valid_test(r_params)
-    if r_params[:stock_id].present? &&  r_params[:title].present? && r_params[:rate].present? && r_params[:review].present?
-      return true
-    else
-      return false
-    end
   end
 
 end
