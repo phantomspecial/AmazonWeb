6eaf201753258b7e2e90be97a1984a50d471de6f
phantomspecial <win.2000@live.jp>
2018-01-10 19:10:21 +0900

    Revise-review-edit1(1)_rewrite

--- app/assets/javascripts/reviewedit.js
+++ app/assets/javascripts/reviewedit.js
@@ -1,61 +1,27 @@
 $(document).on('turbolinks:load', function() {
-  // $("#editreview").click(function()){
-  //    $(this).parent().siblings('#edit-modal').fadeIn();
-  // });
-  // $('.updbtn').on('click',function(){
-  //   $(this).parent().parent().parent('#edit-modal').fadeOut();
-  // });
 
-
-
- $(".editreview").click(function(){
+  $(".editreview").click(function(){
     alert("AAA");
-      $("body").append('<div id="edit-modal"></div>');
-      modalResize();
-        $("#modal-bg,#edit-modal").fadeIn("slow");
-        $("#modal-bg").click(function(){
-          $("#edit-modal,#modal-bg").fadeOut("slow",function(){
-              $('#modal-bg').remove() ;
-          });
-        });
-      $(window).resize(modalResize);
-      function modalResize(){
-        var w = $(window).width();
-        var h = $(window).height();
-        var cw = $("#edit-modal").outerWidth();
-        var ch = $("#edit-modal").outerHeight();
-        $("#edit-modal").css({
-          "left": ((w - cw)/2) + "px",
-          "top": ((h - ch)/2) + "px"
-        });
-      }
-   });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    $("body").append('<div id="#edit-modal"></div>');
+    modalResize();
+    $("#modal-bg, #edit-modal").fadeIn("slow");
+    $("#modal-bg").click(function(){
+      $("#edit-modal,#modal-bg").fadeOut("slow",function(){
+        $('#modal-bg').remove() ;
+      });
+    });
+    $(window).resize(modalResize);
+    function modalResize(){
+      var w = $(window).width();
+      var h = $(window).height();
+      var cw = $("#edit-modal").outerWidth();
+      var ch = $("#edit-modal").outerHeight();
+      $("#edit-modal").css({
+        "left": ((w - cw)/2) + "px",
+        "top": ((h - ch)/2) + "px"
+      });
+    }
+  });
 });
-
-
-// $(document).on('turbolinks:load', function() {
-
-//   $(document).on('click','.edit_review' ,function(){
-//      $(this).parent().siblings('#edit-modal').fadeIn();
-//    });,
-//   $('.updbtn').on('click',function(){
-//      $(this).parent().parent().parent('#edit-modal').fadeOut();
-//    });
-
-// });
 
 
